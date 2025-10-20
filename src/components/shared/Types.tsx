1ec5dc66058476f8dc83d69f4611601955ce6b23
Marcel Stör <marcelstoer@users.noreply.github.com>
2025-10-20 11:08:21 +0200

    Add support for the optional SchemaFragment#objectRefType property (#1)

--- src/components/shared/Types.tsx
+++ src/components/shared/Types.tsx
@@ -1,3 +1,4 @@
+import { getLastPathSegment } from '@stoplight/json';
 import {
   isBooleanishNode,
   isReferenceNode,
@@ -70,6 +71,35 @@ export const Types: React.FunctionComponent<{ schemaNode: SchemaNode }> = ({ sch
     }
 
     printedName ??= type + (formats === null || formats[0] !== type ? '' : `<${formats[1]}>`);
+
+    // It addresses https://github.com/stoplightio/elements/issues/2762.
+    // Abstract: instead of displaying "object" or "array[object]" we want to display the actual type of 'object'.
+    //
+    // The optional 'objectRefType' property is what ti&m Stoplight Elements core supplies for $ref types.
+    // In some cases (which ones?), the type information is available in the standard '$ref' property (evaluate both).
+    //
+    // We cannot redefine the SchemaFragment type as it's defined outside the scope of this project.
+    // -> sprinkle with '@ts-ignore'
+    //
+    if (type === SchemaNodeKind.Array) {
+      // @ts-ignore
+      if (schemaNode.fragment?.items?.objectRefType) {
+        // @ts-ignore
+        printedName = `array[${schemaNode.fragment.items.objectRefType}]`;
+        // @ts-ignore
+      } else if (schemaNode.fragment?.items?.$ref) {
+        // @ts-ignore
+        printedName = getLastPathSegment(schemaNode.fragment.items.$ref as string);
+      }
+    } else if (type === SchemaNodeKind.Object) {
+      // @ts-ignore
+      if (schemaNode.fragment?.objectRefType) {
+        // @ts-ignore
+        printedName = schemaNode.fragment.objectRefType;
+      } else if (schemaNode.fragment?.$ref) {
+        printedName = getLastPathSegment(schemaNode.fragment.$ref as string);
+      }
+    }
 
     return (
       <React.Fragment key={type}>
